f512382945f8f5cf32aa2fe11e242321abb7d76f
Hyper <hyper.jiang@lalamove.com>
2020-07-02 15:07:31 +0800

    increase test coverage

--- birthday_test.go
+++ birthday_test.go
@@ -45,3 +45,143 @@ func TestFormat(t *testing.T) {
 	should.Equal("2/7/2020", birthday.Format("2/1/2006"))
 	should.Equal("Jul 2nd, 2020", birthday.Format("Jan 2nd, 2006"))
 }
+
+func TestParseConstellation(t *testing.T) {
+	should := require.New(t)
+
+	tests := []struct {
+		month         time.Month
+		day           int
+		constellation string
+	}{
+		{
+			time.January,
+			1,
+			"Capricorn",
+		},
+		{
+			time.January,
+			28,
+			"Aquarius",
+		},
+		{
+			time.February,
+			1,
+			"Aquarius",
+		},
+		{
+			time.February,
+			28,
+			"Pisces",
+		},
+		{
+			time.March,
+			1,
+			"Pisces",
+		},
+		{
+			time.March,
+			28,
+			"Aries",
+		},
+		{
+			time.April,
+			1,
+			"Aries",
+		},
+		{
+			time.April,
+			28,
+			"Taurus",
+		},
+		{
+			time.May,
+			1,
+			"Taurus",
+		},
+		{
+			time.May,
+			28,
+			"Gemini",
+		},
+		{
+			time.June,
+			1,
+			"Gemini",
+		},
+		{
+			time.June,
+			28,
+			"Cancer",
+		},
+		{
+			time.July,
+			1,
+			"Cancer",
+		},
+		{
+			time.July,
+			28,
+			"Leo",
+		},
+		{
+			time.August,
+			1,
+			"Leo",
+		},
+		{
+			time.August,
+			28,
+			"Virgo",
+		},
+		{
+			time.September,
+			1,
+			"Virgo",
+		},
+		{
+			time.September,
+			28,
+			"Libra",
+		},
+		{
+			time.October,
+			1,
+			"Libra",
+		},
+		{
+			time.October,
+			28,
+			"Scorpio",
+		},
+		{
+			time.November,
+			1,
+			"Scorpio",
+		},
+		{
+			time.November,
+			28,
+			"Sagittarius",
+		},
+		{
+			time.December,
+			1,
+			"Sagittarius",
+		},
+		{
+			time.December,
+			28,
+			"Capricorn",
+		},
+		{
+			13,
+			1,
+			"Unknown",
+		},
+	}
+
+	for _, test := range tests {
+		should.Equal(test.constellation, parseConstellation(test.month, test.day))
+	}
+}
